9767c540bcefe55cb08fdbbf3f20747f322130e2
lorenzopetrella <petrella.lorenz@gmail.com>
2022-03-22 22:28:01 +0100

    changed BCE loss from percentage to plain 4-digit decimal

--- corso_progetto/custom_libraries/miscellaneous.py
+++ corso_progetto/custom_libraries/miscellaneous.py
@@ -11,14 +11,14 @@ def progress_bar(executed, max=100, pre="", post=""):
 
 def print_fit_history(fit_history, epochs):
     n_epochs = len(fit_history.history['acc'])
-    train_loss = round(fit_history.history['binary_crossentropy'][-1] * 100, 2)
+    train_loss = round(fit_history.history['binary_crossentropy'][-1], 4)
     train_acc = round(fit_history.history['acc'][-1] * 100, 2)
-    valid_loss = round(fit_history.history['val_binary_crossentropy'][-1] * 100, 2)
+    valid_loss = round(fit_history.history['val_binary_crossentropy'][-1], 4)
     valid_acc = round(fit_history.history['val_acc'][-1] * 100, 2)
     print(
-        f"Epochs: {n_epochs}/{epochs} - Train BCE: {train_loss}%, accuracy: {train_acc}% - Validation BCE: {valid_loss}%, accuracy: {valid_acc}%")
+        f"Epochs: {n_epochs}/{epochs} - Train BCE: {train_loss}, accuracy: {train_acc}% - Validation BCE: {valid_loss}, accuracy: {valid_acc}%")
 
 
 def print_evaluate_history(evaluate_history):
     print(
-        f"Test BCE: {round(evaluate_history[1] * 100, 2)}%, accuracy: {round(evaluate_history[2] * 100, 2)}%")
+        f"Test BCE: {round(evaluate_history[1], 4)}, accuracy: {round(evaluate_history[2] * 100, 2)}%")
