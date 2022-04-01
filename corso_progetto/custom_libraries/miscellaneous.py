5a833c8a024ef698844a9e60de358a7b7e621f2b
Lorenzo Petrella <petrella.lorenz@gmail.com>
2022-04-01 16:41:58 +0200

    corrected results printouts during training

--- corso_progetto/custom_libraries/miscellaneous.py
+++ corso_progetto/custom_libraries/miscellaneous.py
@@ -1,6 +1,8 @@
 import sys
 from math import floor
 
+import numpy as np
+
 
 def progress_bar(executed, max=100, pre="", post=""):
     sys.stdout.write('\r')
@@ -10,13 +12,18 @@ def progress_bar(executed, max=100, pre="", post=""):
 
 
 def print_fit_history(fit_history, epochs):
+
+    best_epoch = np.argmin(fit_history.history['val_binary_crossentropy'])
+    if best_epoch.size > 1:
+        best_epoch = best_epoch[0]
+
     n_epochs = len(fit_history.history['acc'])
-    train_loss = round(fit_history.history['binary_crossentropy'][-1], 4)
-    train_acc = round(fit_history.history['acc'][-1] * 100, 2)
-    valid_loss = round(fit_history.history['val_binary_crossentropy'][-1], 4)
-    valid_acc = round(fit_history.history['val_acc'][-1] * 100, 2)
+    train_loss = round(fit_history.history['binary_crossentropy'][best_epoch], 4)
+    train_acc = round(fit_history.history['acc'][best_epoch] * 100, 2)
+    valid_loss = round(fit_history.history['val_binary_crossentropy'][best_epoch], 4)
+    valid_acc = round(fit_history.history['val_acc'][best_epoch] * 100, 2)
     print(
-        f"Epochs: {n_epochs}/{epochs} - Train BCE: {train_loss}, accuracy: {train_acc}% - Validation BCE: {valid_loss}, accuracy: {valid_acc}%")
+        f"Epochs: {n_epochs}/{epochs} (best = {best_epoch+1}) - Train BCE: {train_loss}, accuracy: {train_acc}% - Validation BCE: {valid_loss}, accuracy: {valid_acc}%")
 
 
 def print_evaluate_history(evaluate_history):
