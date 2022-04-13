2dffd076d37eff322173c0e50a463d8b5d8db8dc
Lorenzo Petrella <petrella.lorenz@gmail.com>
2022-04-13 14:28:39 +0200

    uploaded csv results

--- corso_progetto/results/pptx_data/np2csv.py
+++ corso_progetto/results/pptx_data/np2csv.py
@@ -1,4 +1,4 @@
 import numpy as np
 
-arr = np.load("ktree_history_nobias.npy", allow_pickle=True)
-arr.tofile("ktree_history_nobias.csv", sep=";")
+arr = np.load("ktree_history_trees_nobias.npy", allow_pickle=True)
+arr.tofile("ktree_history_trees_nobias.csv", sep=";")
